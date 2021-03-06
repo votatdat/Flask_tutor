7f614735826cc8befc21f8f034e098e9923df005
Vo Tat Dat <tatdat13@gmail.com>
2021-03-06 12:02:30 +0700

    Added RegistrationForm and LoginForm

--- flask_blog.py
+++ flask_blog.py
@@ -1,8 +1,12 @@
+import os
+
 from flask import Flask, render_template, url_for
 
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 posts = [
     {
@@ -24,10 +28,22 @@ title = "Dat"
 
 @app.route('/')
 @app.route('/home')
-def hello_world():
+def home():
     return render_template('home.html', posts=posts, title=title)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
