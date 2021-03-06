bfee541233fe38d8bc9e7940e7565ed5195814ac
Vo Tat Dat <tatdat13@gmail.com>
2021-03-06 10:47:53 +0700

    added dot-html templates

--- flask_blog.py
+++ flask_blog.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'Dat Vo',
+        'title': 'First blog',
+        'content': 'This is the first blog.',
+        'date_posted': '2020, Aug, 13'
+    },
+    {
+        'author': 'Thien Pham',
+        'title': 'Second blog',
+        'content': 'This is the second blog.',
+        'date_posted': '2021, Jan, 15'
+    }
+]
+
+title = "Dat"
+
+
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return render_template('home.html', posts=posts, title=title)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
