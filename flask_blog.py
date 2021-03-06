1fc61bfbb4ba135455b83d60b4843e73a2367e73
Vo Tat Dat <tatdat13@gmail.com>
2021-03-06 13:11:08 +0700

    Finished part3: Form

--- flask_blog.py
+++ flask_blog.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, flash
 
 from forms import RegistrationForm, LoginForm
 
@@ -37,13 +37,24 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@admin.com' and form.password.data == 'admin':
+            flash(f'You have been logged in.', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login Unsuccessful. Please try again.', 'danger')
+
     return render_template('login.html', title='Login', form=form)
